623f8408101ea8982c39432ec6381e1726e30f57
Oleg S V <osidorkin85@gmail.com>
2023-01-31 16:56:51 +0300

    save fix

--- src/components/diary-entry.tsx
+++ src/components/diary-entry.tsx
@@ -1,15 +1,16 @@
 "use client"
 import '../app/globals.css';
-import {useEffect, useState} from "react";
+import {useEffect, useLayoutEffect, useRef, useState} from "react";
 import {Temporal} from "@js-temporal/polyfill";
 
 export default function DiaryEntry({
 									   setTimelineWordcount,
 									   initialText
-	}: {
-		setTimelineWordcount: (wc: number) => void,
-		initialText: string
-	}) {
+								   }: {
+	setTimelineWordcount: (wc: number) => void,
+	initialText: string
+}) {
+	const textAreaRef = useRef<HTMLTextAreaElement>(null)
 
 	const [text, setText] = useState(initialText)
 	const [isSaved, setIsSaved] = useState(true)
@@ -23,35 +24,55 @@ export default function DiaryEntry({
 		return wordsArr.length;
 	}
 
+	useEffect(() => {
+		const scrollLeft = window.scrollX;
+		const scrollTop = window.scrollY;
+
+		if (textAreaRef.current) {
+			textAreaRef.current.style.overflow = 'hidden';
+			textAreaRef.current.style.height = "auto";
+			textAreaRef.current.style.height = `${Math.max(
+				textAreaRef.current.scrollHeight,
+				160
+			)}px`;
+		}
+		window.scrollTo(scrollLeft, scrollTop);
+	}, [text]);
+
+	useEffect(() => {
+		setTimelineWordcount(getWordsCount())
+		// setRows(Math.max(text.split("\n").length + 1, 10))
+	}, [text])
+
 	function onChangeText(e: React.FormEvent<HTMLTextAreaElement>) {
 		setText(e.currentTarget.value)
 		setIsSaved(false)
-		setTimelineWordcount(getWordsCount())
 	}
 
-	async function onSave(e: React.KeyboardEvent) {
-		if (e.key === 's' && e.ctrlKey) {
-			e.preventDefault()
-			e.stopPropagation()
-
-			setSaving(true)
-			await fetch('/api/today', {
-				method: "POST",
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({
-					text: text,
-					date: Temporal.Now.zonedDateTimeISO().toPlainDate().toString(),
-					word_count: getWordsCount()
-				})
-			}).then(() => {
-				setIsSaved(true)
-			}).finally(() => {
-				setSaving(false)
+	async function onSave() {
+		setSaving(true)
+		await fetch('/api/today', {
+			method: "POST",
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				text: text,
+				date: Temporal.Now.zonedDateTimeISO().toPlainDate().toString(),
+				word_count: getWordsCount()
 			})
-			return false
-		}
+		}).then(() => {
+			setIsSaved(true)
+		}).finally(() => {
+			setSaving(false)
+		})
+	}
+
+	function putTab(e: React.KeyboardEvent<HTMLTextAreaElement>) {
+		const start = e.currentTarget.selectionStart;
+		const end = e.currentTarget.selectionEnd;
+		setText(text.substring(0, start) + '\t' + text.substring(end));
+		e.currentTarget.selectionStart = e.currentTarget.selectionEnd = start + 1;
 	}
 
 	return (
@@ -59,13 +80,15 @@ export default function DiaryEntry({
 			<h2 className="text-blue-50 text-3xl self-start w-full">Автор, жги!</h2>
 
 			{saving &&
-                <img alt="loading" width="16" height="16" src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA=="/>
+                <img alt="loading" width="16" height="16"
+                     src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA=="/>
 			}
 			{isSaved && <span>Сохранено</span>}
 			{!isSaved && <span className="text-red-100">Изменено, но пока не сохранено</span>}
 			<textarea
+				ref={textAreaRef}
 				className="overflow-hidden w-full text-gray-800 mt-5
-				text-2xl leading-10 pt-12 px-24 pb-9 bg-local bg-blue-300
+				text-2xl leading-10 pt-[50px] px-24 pb-9 mb-16 bg-local bg-blue-300
 				rounded-xl shadow-lg border-t-2 border-b-2 border-white max-w-screen-xl"
 				style={{background: "url('/bg/lines.png') repeat-y, url('/bg/paper.png') repeat"}}
 				value={text}
@@ -75,11 +98,24 @@ export default function DiaryEntry({
 				rows={10}
 				name="entry"
 				autoFocus={true}
-				onKeyDown={onSave}
-				onKeyUp={(e) => {
-					if (e.key === 's' && e.ctrlKey) {
+				onKeyDown={(e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+					if (e.ctrlKey && (e.key === 's' || e.key === 'ы')) {
 						e.preventDefault()
 						e.stopPropagation()
+						onSave()
+						return false
+					}
+					if (e.key === 'Tab') {
+						e.preventDefault()
+						e.stopPropagation()
+						putTab(e)
+					}
+				}}
+				onKeyUp={(e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+					if (e.ctrlKey && (e.key === 's' || e.key === 'ы')) {
+						e.preventDefault()
+						e.stopPropagation()
+						return false
 					}
 				}}
 			/>
--- todo.md
+++ todo.md
@@ -1,26 +1,24 @@
 BACKLOG
 - [ ] Реализовать api
-  - [ ] POST /api/today сохраняет запись
   - [ ] Реализовать запрос к базе данных за timeline не текущего месяца
   - [ ] /[yyyymm] - отрисовывает Timeline и DiaryEntry textarea если сегодня
 - [ ] реализовать страницы
     - [ ] реализовать листание Timeline по месяцам (isCurrentMonth)
   - [ ] /[yyyymmdd] - отрисовывает Timeline месяца и DiaryEntry
     - [ ] реализовать компонент DiaryEntry с props {date, text} для отображения прошлых записей
-- [ ] * пофиксить шифт временных зон 
 - [ ] написать добавление записи
   - [ ] реализовать DiaryEntry с textarea для текущего дня
-    - [ ] autosize textarea
-    - [ ] loader
-    - [ ] onSave - по интервалу
-    - [ ] onSave - по Ctrl_S
+  - [ ] не сохранять если другой день
+  - [ ] реализовать функцию взять координаты каретки https://hashnode.com/post/how-do-you-get-the-position-of-the-cursor-in-pixelsinside-a-textarea-cje14wjck0d1om3wtvfem6ofg
+  - [ ] onSave - по интервалу
+- [ ] * пофиксить шифт временных зон
 
-      Девочки
+    Девочки
 - [ ] Сверстать компонент Timeline
 
 
 
-TODO 30.01.23
+  TODO 30.01.23
 
   DONE
 - [x] Реализовать запрос к базе данных за timeline текущего месяца
@@ -31,3 +29,7 @@ TODO 30.01.23
 - [x] разобраться с добавлением динамики на страницы
 - [x] Перенести страницы в app
 - [x] отрисовывать количество слов на таймлайне
+- [x] autosize textarea
+- [x] onSave - по Ctrl_S
+- [x] loader
+- [x] POST /api/today сохраняет запись
