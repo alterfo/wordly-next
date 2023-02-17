9608ee2cd09aa1343e333d1815b3dd1d1cec0316
Oleg S V <osidorkin85@gmail.com>
2023-02-17 06:55:05 +0300

    redirect to today after login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,6 +2,7 @@ import 'server-only'
 import {createClient} from "@/supabase-server";
 import {redirect} from "next/navigation";
 import GithubLoginButton from "@/components/github-login-button";
+const Temporal = require("@js-temporal/polyfill").Temporal;
 
 export default async function LoginPage() {
 	const supabase = createClient()
@@ -11,7 +12,7 @@ export default async function LoginPage() {
 	} = await supabase.auth.getSession()
 
 	if (session) {
-		redirect('/')
+		redirect('/' + Temporal.Now.plainDateISO().toString()) // todo: redirectedFrom
 	}
 
 	return <>
